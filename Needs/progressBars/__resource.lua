e35ff66cc37a312d9ea0b19c18095f2d955451c1
SamuelTVx <121121539+SamuelTVx@users.noreply.github.com>
2022-12-20 18:43:11 +0100

    Add files via upload

---
+++ Needs/progressBars/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ui_page 'h.html'
+author 'SamuelTV <3'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'h.html'
+}
+
+exports {
+  'startUI',
+  'closeUI',
+}
